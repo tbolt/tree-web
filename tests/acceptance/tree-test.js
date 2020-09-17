086efdd2782d3d3610bbc277117820ec353737d9
Ty Bolchoz <tybolt@gmail.com>
2020-09-17 18:51:53 -0400

    updates

---
+++ tests/acceptance/tree-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | tree', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('p').hasText("The Social Network that doesn't track you.");
+  });
+});
